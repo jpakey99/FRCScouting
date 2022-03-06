d6c03dcd839226df0d457680ebb69a54a8bc5c6d
John Akey <jpakey99@gmail.com>
2022-03-05 21:16:55 -0500

    Automatically retrieving the playoff alliance.  This will be used to prdict playoff matches and see if team contribution (offense) can indicate who is going to win the best of 3.  This will also be used to see how effecient drafting is (see how many points they "left" on the table

--- event_data_gathering.py
+++ event_data_gathering.py
@@ -4,7 +4,19 @@ import data2022
 import VisCreation
 from scouting.scouting2022 import scouting2022, team_point_contribution
 tba = tbapy.TBA('rzxV1jZwdmWsmJGakoQrdFmCVntwtcGtSPcaVCEjWXXW8wpoScnXWUsFCJ1mY3n9')
+
 from scouting.power_rankings import power_rankings2022
+
+
+def get_playoff_allainces(event):
+    alliances = []
+    raw_alliances = tba.event_alliances(event)
+    for a in raw_alliances:
+        alliance = []
+        for team in a['picks']:
+            alliance.append(team)
+        alliances.append(alliance)
+    return alliances
 
 
 def qual_matches(event_matches):
@@ -24,20 +36,16 @@ def playoff_matches(event_matches):
 
 
 data_dict = data2022.create_data_dict_2022()
-event = "2022week0"
-match = tba.event_matches(event)
-match = qual_matches(match)
-team_dict = scouting2022(match)
-cont = team_point_contribution(team_dict)
-print(cont)
-power_rankings2022(cont)
+event = "2022flwp"
+print(get_playoff_allainces(event))
+# match = tba.event_matches(event)
+# match = qual_matches(match)
+# team_dict = scouting2022(match)
+# cont = team_point_contribution(team_dict)
+# print(cont)
+# power_rankings2022(cont)
 # print(match)
-# keys = match['score_breakdown']['blue']
-# for m in match:
-#     print(m['comp_level'], m['key'])
-    # data2022.match_data_20222(m, data_dict)
 
-# VisCreation.general_points_vis([data_dict])
 
 # print([data_dict])
 # print(len(data_dict['general']['autoPoints']))
